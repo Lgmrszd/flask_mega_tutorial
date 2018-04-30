15d3e39a885d53640dad6f8b77bf50c0d89d50f7
Mikhail <m.v.bobrov@yandex.ru>
2018-04-30 17:14:40 +0300

    new run files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app, __version__
-from flask import render_template, flash, redirect
-from app.forms import LoginForm, CoderForm
+from flask import render_template, flash, redirect, g
+from app.forms import LoginForm, CoderForm, MelodyEscapeHelperForm
 from app.functions import do_pipeline
+from werkzeug.utils import secure_filename
+import os
 
 
 @app.route('/')
@@ -37,3 +39,18 @@ def coder():
                            title='Coder',
                            form=form,
                            coders=app.config['CODERS'])
+
+
+@app.route('/MelodyEscapeHelper', methods=['GET', 'POST'])
+def upload():
+    form = MelodyEscapeHelperForm()
+    if form.validate_on_submit():
+        f = form.image.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(
+            app.config["IMAGES_DIR"], filename
+        ))
+        return redirect('/index')
+
+    return render_template('ME_helper.html',
+                           form=form)
--- run_server.py
+++ run_server.py
@@ -1,3 +1,3 @@
 from app import app
-from app import views
+from app import routes
 app.run(host="0.0.0.0", debug=True)
